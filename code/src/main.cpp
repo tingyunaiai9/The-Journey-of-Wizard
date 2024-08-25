30dec4e1f9abda71fd011f8f6c5ca6ed47d11ad1
tingyunaiai9 <2920825585@qq.com>
2024-08-25 21:58:29 +0800

    Ensure the application’s adaptability and visual consistency across different resolution displays
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -2,6 +2,9 @@
 #include "MyGame.h"
 
 int main(int argc, char *argv[]) {
+    // ensure the application’s adaptability and visual consistency across different resolution displays
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::Floor);
+
     QApplication a(argc, argv);
     MyGame myGame;
     myGame.show();
