0fb9bd044259710700968df8a2fa3715dd977732
tingyunaiai9 <2920825585@qq.com>
2024-08-26 10:01:44 +0800

    change background
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- code/src/Items/Maps/Battlefield.cpp
+++ code/src/Items/Maps/Battlefield.cpp
@@ -4,7 +4,7 @@
 
 #include "Battlefield.h"
 
-Battlefield::Battlefield(QGraphicsItem *parent) : Map(parent, ":/Items/Maps/Battlefield/g9tOqth.png") {}
+Battlefield::Battlefield(QGraphicsItem *parent) : Map(parent, ":/Items/Maps/Battlefield/forest.png") {}
 
 qreal Battlefield::getFloorHeight() {
     auto sceneRect = sceneBoundingRect();
