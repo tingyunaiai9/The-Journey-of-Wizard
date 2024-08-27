9d5f6952768a497b570c1c44c22d0639dba3fd90
tingyunaiai9 <2920825585@qq.com>
2024-08-27 11:11:04 +0800

    delete equipments from assistant
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- code/src/Items/HeadEquipments/CapOfTheHero.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by gerw on 8/20/24.
-//
-
-#include "CapOfTheHero.h"
-
-CapOfTheHero::CapOfTheHero(QGraphicsItem *parent) : HeadEquipment(parent,
-                                                                  ":/Items/HeadEquipments/CapOfTheHero/BotW_Cap_of_the_Hero_Icon.png") {}
--- code/src/Items/LegEquipments/WellWornTrousers.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by gerw on 8/20/24.
-//
-
-#include "WellWornTrousers.h"
-
-WellWornTrousers::WellWornTrousers(QGraphicsItem *parent) : LegEquipment(parent, ":/Items/LegEquipments/WellWornTrousers/BotW_Well-Worn_Trousers_Icon.png") {
-}
