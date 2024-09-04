feb7fabe91472917128917559c05daeca349d645
tingyunaiai9 <2920825585@qq.com>
2024-09-04 19:46:10 +0800

    update part 6 of readme
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- code/src/Scenes/Scene.cpp
+++ code/src/Scenes/Scene.cpp
@@ -5,17 +5,19 @@ Scene::Scene(QObject *parent) : QGraphicsScene(parent), timer(new QTimer(this)) 
     connect(timer, &QTimer::timeout, this, &Scene::update);
 }
 
-void Scene::update() {
+void Scene::update()
+{
     auto currentTime = QDateTime::currentDateTime().toMSecsSinceEpoch();
     if (lastTime == -1) { // first frame
         deltaTime = 0;
     } else {
         deltaTime = currentTime - lastTime;
+        // qDebug() << "deltaTime: " << deltaTime;
     }
     lastTime = currentTime;
-
 }
 
-void Scene::startLoop() {
+void Scene::startLoop()
+{
     timer->start(1000 / 90); // 90FPS
 }
