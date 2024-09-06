211ea1d36b0585ebf780e9a39f57ea7c91b7b33b
tingyunaiai9 <2920825585@qq.com>
2024-09-06 17:33:00 +0800

    beautify gameover scene
    Signed-off-by: tingyunaiai9 <2920825585@qq.com>

--- code/src/Scenes/GameOverScene.cpp
+++ code/src/Scenes/GameOverScene.cpp
@@ -1,7 +1,7 @@
 #include "GameOverScene.h"
-#include <QGraphicsTextItem>
-#include <QFont>
-#include <QColor>
+
+#include <QGraphicsProxyWidget>
+
 
 GameOverScene::GameOverScene(const QString& winnerText, QObject *parent)
     : QGraphicsScene(parent)
@@ -11,28 +11,52 @@ GameOverScene::GameOverScene(const QString& winnerText, QObject *parent)
 
 void GameOverScene::createUI(const QString& winnerText)
 {
-    // 设置场景大小
     setSceneRect(0, 0, 1280, 720);
 
-    // 胜利者文本
-    QGraphicsTextItem* victoryText = new QGraphicsTextItem(winnerText);
-    QFont font;
-    font.setPointSize(32);  // 设置字体大小
-    font.setBold(true);     // 设置粗体
-    victoryText->setFont(font);
-    victoryText->setDefaultTextColor(QColor("#72a5a2"));  // 设置文本颜色
-    victoryText->setPos(400, 200);  // 设置文本的位置
-    addItem(victoryText);
+    // picture
+    if (winnerText == "Player 1 Wins!")
+    {
+        m_pixmap = new QPixmap(":/Scenes/GameOverScene/gameover_player1_win.png");
+    }
+    else if (winnerText == "Player 2 Wins!")
+    {
+        m_pixmap = new QPixmap(":/Scenes/GameOverScene/gameover_player2_win.png");
+    }
+    else
+    {
+        m_pixmap = new QPixmap(":/Scenes/GameOverScene/gameover.png");
+    }
 
-    // 返回主菜单按钮
-    QPushButton* returnButton = new QPushButton("返回主菜单");
-    returnButton->setGeometry(500, 350, 200, 50);
+    *m_pixmap = m_pixmap->scaled(sceneRect().size().toSize(), Qt::KeepAspectRatio);
+    m_pixmapItem = new QGraphicsPixmapItem(*m_pixmap);
+    addItem(m_pixmapItem);
+
+    // button
+    returnButton = new QPushButton("Main Menu");
+    returnButton->setFixedSize(200, 50);
+    returnButton->setStyleSheet("QPushButton {"
+                                "background-color: rgba(255, 255, 255, 10);"
+                                "border: 2px solid rgba(114, 165, 162, 100);"
+                                "border-radius: 10px;"
+                                "color: #72a5a2;"
+                                "font-size: 30px;"
+                                "}");
+    QGraphicsProxyWidget *returnButtonProxy = this->addWidget(returnButton);
+    returnButtonProxy->setPos(640 - returnButton->width() / 2, 360 - returnButton->height() / 2 + 50);
+    returnButtonProxy->setZValue(1);
     connect(returnButton, &QPushButton::clicked, this, &GameOverScene::returnToMainMenu);
-    addWidget(returnButton);
 
-    // 退出游戏按钮
-    QPushButton* exitButton = new QPushButton("退出游戏");
-    exitButton->setGeometry(500, 450, 200, 50);
+    exitButton = new QPushButton("Exit Game");
+    exitButton->setFixedSize(200, 50);
+    exitButton->setStyleSheet("QPushButton {"
+                              "background-color: rgba(255, 255, 255, 10);"
+                              "border: 2px solid rgba(114, 165, 162, 100);"
+                              "border-radius: 10px;"
+                              "color: #72a5a2;"
+                              "font-size: 30px;"
+                              "}");
+    QGraphicsProxyWidget *exitButtonProxy = this->addWidget(exitButton);
+    exitButtonProxy->setPos(640 - exitButton->width() / 2, 360 - exitButton->height() / 2 + 150);
+    exitButtonProxy->setZValue(1);
     connect(exitButton, &QPushButton::clicked, this, &GameOverScene::exitGame);
-    addWidget(exitButton);
 }
--- code/src/Scenes/GameOverScene.h
+++ code/src/Scenes/GameOverScene.h
@@ -1,12 +1,21 @@
-#ifndef GAMEOVERSCENE_H
-#define GAMEOVERSCENE_H
+#ifndef QT_PROGRAMMING_2024_GAMEOVERSCENE_H
+#define QT_PROGRAMMING_2024_GAMEOVERSCENE_H
 
 #include <QGraphicsScene>
 #include <QPushButton>
+#include <QPixmap>
+#include <QGraphicsPixmapItem>
 
 class GameOverScene : public QGraphicsScene
 {
     Q_OBJECT
+
+private:
+    QPushButton* returnButton = nullptr;
+    QPushButton* exitButton = nullptr;
+
+    QPixmap* m_pixmap = nullptr;
+    QGraphicsPixmapItem* m_pixmapItem = nullptr;
 
 public:
     explicit GameOverScene(const QString& winnerText, QObject *parent = nullptr);
@@ -19,4 +28,4 @@ private:
     void createUI(const QString& winnerText);
 };
 
-#endif // GAMEOVERSCENE_H
+#endif // QT_PROGRAMMING_2024_GAMEOVERSCENE_H
